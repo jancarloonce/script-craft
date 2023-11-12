56aee61a4587e41ebc0a56f702f6c47babbadb97
jancarloonce <jancarloonce11@gmail.com>
2023-11-12 20:27:25 +0800

    Migrated changes from replit to github

--- static/script.js
+++ static/script.js
@@ -1,4 +1,17 @@
-// static/script.js
+// script.js
+
+$(document).ready(function () {
+    $('#productForm').submit(function () {
+        // Show loading spinner
+        $('#loading-spinner').css('display', 'block');
+        // Disable the submit button to prevent multiple submissions
+        $('#submit-button').prop('disabled', true);
+    });
+});
+
+function showCardDetail(scriptIdea) {
+    // Code for showing card details, if needed
+}
 
 function toggleCardText(button) {
     var cardContent = button.parentElement.querySelector('.card-text');
@@ -11,4 +24,8 @@ function toggleCardText(button) {
         cardContent.style.maxHeight = '150px'; /* Show limited text */
         button.textContent = 'Show More';
     }
+}
+
+function closeModal() {
+    // Code for closing the modal, if needed
 }
